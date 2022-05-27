c4ce4d8de46998475c9d56babe1fd896c2fc3262
Anchitha Kotchasila <mb.armi.anchitha@gmail.com>
2022-05-27 16:32:56 +0700

    first commit

---
+++ api/routes/root.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const rootController = require('../controllers/root')
+
+const { requireLogin } = require('../controllers/user')
+
+
+const router = express.Router()
+
+router.post('/post-bisection',requireLogin,rootController.postBisection)
+
+router.get('/get-bisection',requireLogin,rootController.getBisection)
+
+module.exports = router
